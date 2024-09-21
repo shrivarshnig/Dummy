810d680388ab709295d3547f072a6ff327198eee
shrivarshnig <shrivarshnig@gmail.com>
2024-09-21 12:00:53 +0530

    version 1.0 Ovservation:code for data_rate

---
+++ receiverate.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 512  // Size of the buffer for receiving data
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("Set socket options error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind the socket to the port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening for connections
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receiving data
+    while (1) {
+        ssize_t bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received <= 0) {
+            perror("Receive failed");
+            break;
+        }
+        // Process the received data
+        printf("Received %ld bytes\n", bytes_received);
+
+        // Send acknowledgment back to the client
+        const char *ack = "ACK";
+        send(new_socket, ack, strlen(ack), 0);
+    }
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
